a75bdfe6475855d6a5a32b635c59a9db96ac5c65
Charles Yao <chongbinyao33@gmail.com>
2025-10-01 21:11:15 -0500

    Initial Commit

---
+++ LangFred/LangFredApp.swift
@@ -0,0 +1,17 @@
+//
+//  LangFredApp.swift
+//  LangFred
+//
+//  Created by Chongbin Yao on 10/1/25.
+//
+
+import SwiftUI
+
+@main
+struct LangFredApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LangFredTests/LangFredTests.swift
@@ -0,0 +1,16 @@
+//
+//  LangFredTests.swift
+//  LangFredTests
+//
+//  Created by Chongbin Yao on 10/1/25.
+//
+
+import Testing
+
+struct LangFredTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
